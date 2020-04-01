354e381143530994c2d6d0ad2e6c2a3123c50ed4
Kashee Ram Kushwaha <kashee.nitd2016@gmail.com>
2020-04-01 21:25:45 +0530

    Initial Commit

---
+++ pageTapper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pageTapper
+//
+//  Created by Kashee Kushwaha on 01/04/20.
+//  Copyright © 2020 Kashee Kushwaha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
